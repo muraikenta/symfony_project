63e40126eaced6fe0fcabdc1c09984259efa13cf
muraikenta <muraikenta0507@gmail.com>
2014-04-23 16:52:52 +0900

    initial commit

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+// symfony directories
+$sf_symfony_lib_dir  = '/Applications/MAMP/bin/php/php5.3.27/lib/php/symfony';
+$sf_symfony_data_dir = '/Applications/MAMP/bin/php/php5.3.27/lib/php/data/symfony';
